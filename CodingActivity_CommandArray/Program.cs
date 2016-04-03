bdfcf9b4ee71d17cd789d6fa5ba2818682576f58
John Velis <john@johnvelis.com>
2016-04-03 10:11:15 -0400

    initial commit

---
+++ CodingActivity_CommandArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_CommandArray
+{
+    class Program
+    {
+        #region GLOBALS
+        private enum FinchCommand
+        {
+            MOVEFORWARD,
+            MOVEBACKWARD,
+            STOP,
+            DELAY,
+            TURNRIGHT,
+            TURNLEFT,
+            LEDON,
+            LEDOFF,
+            TERMINATE
+        }
+
+        private const int NUMBER_OF_COMMNANDS = 5;
+        private const int COMMAND_DURATION = 5;
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+           FinchCommand[] commands = new FinchCommand[NUMBER_OF_COMMNANDS];
+        }
+    }
+}
